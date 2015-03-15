c801c8387cd52690b18bcaa75eab3233c52874c3
sidho <whoissidho@gmail.com>
2015-03-14 20:11:59 -0700

    Get rooms working

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -9,8 +9,10 @@
 
   Chat.prototype.handleCommand = function (command) {
     var commandParams = command.split(' ');
-    if (commandParams[0] === "/nick") {
+    if (commandParams[0] === "/name") {
       this.socket.emit('nicknameChangeRequest', commandParams[1]);
+    } else if (commandParams[0] === "/join") {
+      this.socket.emit('roomChangeRequest', commandParams[1]);
     } else {
       this.socket.emit('message', { text: "Invalid command."})
     }
